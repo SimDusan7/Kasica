4954376a2afb95153fdea3dab49063ac339b61c5
Dusan Simic <dusan.simic@elfak.rs>
2024-02-12 11:18:40 +0100

    da

---
+++ Kasica/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: HostingStartup(typeof(Kasica.Areas.Identity.IdentityHostingStartup))]
+namespace Kasica.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+
+            builder.ConfigureServices((services) =>
+            {
+                services.AddLogging(builder =>
+                {
+                    builder.AddDebug()
+                        .AddConsole()
+                        .SetMinimumLevel(LogLevel.Warning)
+                        .AddFilter("Aguacongas.Identity.Redis", LogLevel.Trace);
+                });
+            });
+
+        }
+    }
+}
